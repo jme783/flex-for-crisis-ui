e6632f43c87b5c2299154baf5d7a404c96d7d558
Jeff Eiden <jeff.m.eiden@gmail.com>
2019-10-21 16:11:15 -0700

    Anonymize the UI

--- src/FfcDemoUiPlugin.js
+++ src/FfcDemoUiPlugin.js
@@ -6,6 +6,17 @@ import CustomThemeOverrides from './CustomThemeOverrides.js';
 
 const PLUGIN_NAME = 'FfcDemoUiPlugin';
 
+const infoPanelContent = `
+  <h1>Conversation Info</h1>
+
+  <h2>Channel</h2>
+  <p>{{task.channelType}}</p>
+  <h2>Created at</h2>
+  <p>{{task.dateCreated}}</p>
+  <h2>Status</h2>
+  <p>{{task.status}}</p>
+`;
+
 export default class FfcDemoUiPlugin extends FlexPlugin {
   constructor() {
     super(PLUGIN_NAME);
@@ -23,8 +34,43 @@ export default class FfcDemoUiPlugin extends FlexPlugin {
 
     // Update the styling of the page to use the right colors
     manager.updateConfig(CustomThemeOverrides);
-
+    // Change the Logo
     flex.MainHeader.defaultProps.logoUrl = "https://sinopia-penguin-7201.twil.io/assets/hope-for-tomorrow-logo.svg"; 
+
+    // Templated UI
+    flex.Manager.getInstance().strings.TaskHeaderLine = "Anonymous";
+    flex.Manager.getInstance().strings.SupervisorTaskHeaderLine = "Anonymous";
+    flex.Manager.getInstance().strings.SupervisorTaskCardHeader = "Anonymous";
+
+    flex.Manager.getInstance().strings.PredefinedChatMessageAuthorName = "Hope For Tomorrow Chatbot";
+    flex.Manager.getInstance().strings.PredefinedChatMessageBody = "Thank you for contacting Hope For Tomorrow's crisis hotline. We want you to know that you are not alone, and we are here to listen.";
+    
+
+    flex.Manager.getInstance().strings.ChatWelcomeText = "Conversation started";
+    flex.Manager.getInstance().strings.WelcomeMessage = "Welcome to Hope For Tomorrow's 24/7 Crisis Hotline";
+    flex.Manager.getInstance().strings.TaskHeaderEndChat = "END CONVERSATION";
+    
+    manager.strings.TaskInfoPanelContent = infoPanelContent;
+    manager.strings.SupervisorTaskInfoPanelContent = infoPanelContent;
+
+    // Chat UI
+
+    flex.MessagingCanvas.defaultProps.memberDisplayOptions = {
+      yourDefaultName: 'You',
+      theirDefaultName: 'Anonymous Person',
+      yourFriendlyNameOverride: false,
+      theirFriendlyNameOverride: false
+    };
+
+    flex.MessageListItem.defaultProps.authorName = "Foo";
+    flex.MessageListItem.defaultProps.useFriendlyName = true;
+
+    flex.MessagingCanvas.defaultProps.avatarCallback = (identity) => {
+      // This is the identity if the message comes from Studio. Change the icon to be a robot
+      if(identity.startsWith('CH')) {
+        return "https://brand.twilio.com/assets/icons/product/product-icon-autopilot.svg"
+      }
+    }
   }
 
   /**
